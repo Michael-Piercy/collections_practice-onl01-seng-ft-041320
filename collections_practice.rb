893486a917b89fdd9da46466f679d5effecfee50
Michael Piercy <michael.piercy@zoho.com>
2020-04-19 20:15:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(num)
+  num.sort
+end
 
+def sort_array_desc(num)
+  num.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(string)
+  string.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array.sort array[1], array[2] = array[2], array[1]
+end
